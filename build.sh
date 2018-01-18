eb04cc78e996e0112a53793ba5fe19a46ba67b48
Pierre Colle <piercus@gmail.com>
2018-01-18 14:05:34 +0100

    rename dists

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ for NODE_VERSION in "${NODE_VERSIONS[@]}"
 do
 	for OPENCV_VERSION in "${OPENCV_VERSIONS[@]}" 
 	do
-		TAG=${OPENCV_VERSION}-${NODE_VERSION}
+		TAG=${NODE_VERSION}-${OPENCV_VERSION}
 		mkdir -p dist/$TAG
 		sed "s/OPENCV_VERSION_TO_REPLACE/${OPENCV_VERSION}/" src/Dockerfile > dist/$TAG/tmp1
 		sed "s/NODE_VERSION_TO_REPLACE/${NODE_VERSION}/" dist/$TAG/tmp1 > dist/$TAG/Dockerfile
