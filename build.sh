7d660ca30ad7a28c24c1a1c3fea3a3f2de2f77e6
Pierre Colle <piercus@gmail.com>
2018-06-22 12:36:41 +0200

    add 2.4.13.6

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 NODE_VERSIONS=('6.10.3' '4.3.2')
 
-OPENCV_VERSIONS=('3.3.1' '3.4.0' '2.4.13.4' '2.4.12.3')
+OPENCV_VERSIONS=('3.3.1' '3.4.0' '2.4.13.4' '2.4.12.3' '2.4.13.6')
 
 for NODE_VERSION in "${NODE_VERSIONS[@]}" 
 do
